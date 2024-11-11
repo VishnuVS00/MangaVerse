94111b51f4589998184f0443018fa7fa740116a4
Vishnu V S <vishnuvs@techversantinfotech.com>
2024-11-11 10:59:22 +0530

    home manga and latest

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,8 +14,11 @@ function Home() {
     // dispatch(fetchManga());
   }, [])
 
+  const popularList = [1, 2]
+  const latestList = [1, 2, 3, 4, 5]
+
   return (
-    <div className='bg-black p-3 h-[80vh]'>
+    <div className='bg-black p-3 pb-5 relative top-[10vh] mb-[20vh]'>
       <input
         type="text"
         placeholder='Search'
@@ -23,7 +26,31 @@ function Home() {
       />
 
       <section>
+        <h2 className='text-white text-2xl font-bold my-2'>Popular</h2>
+        <div className='border border-white h-[50vh] w-full flex gap-3 rounded-md overflow-x-auto p-2 '>
+          {
+            popularList.map((p) => (
+              <div className='border border-white rounded-md w-full min-w-96'>
+                <p className='text-white'>{p}</p>
+              </div>
+            )
+            )
+          }
+        </div>
+      </section>
 
+      <section>
+        <h2 className='text-white text-2xl font-bold my-2'>Latest</h2>
+        <div className='border border-white h-[50vh] flex gap-3 rounded-md overflow-x-auto p-2'>
+          {
+            latestList.map((p) => (
+              <div className='border border-white rounded-md w-screen min-w-96'>
+                <p className='text-white'>{p}</p>
+              </div>
+            )
+            )
+          }
+        </div>
       </section>
     </div>
   )
